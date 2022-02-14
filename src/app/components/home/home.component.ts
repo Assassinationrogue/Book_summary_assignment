6375fa161c06962be72b741561a0ee7abb0f2d86
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-02-14 19:35:28 +0530

    # layout implementation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { fromEvent, merge } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  isLoading: Boolean = false;
+  apiResponse: any = [];
 
-  constructor() { }
+  constructor(public searchInput: ElementRef) { 
+    this.searchInput = searchInput;
+  }
 
   ngOnInit(): void {
+    const keyupEvent$ = fromEvent(this.searchInput?.nativeElement, "keyup");
+    const pasteEvent$ = fromEvent(this.searchInput?.nativeElement, "input");
+    const allEvents$ = merge(keyupEvent$, pasteEvent$);
   }
+
+  
 
 }
