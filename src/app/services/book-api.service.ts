9b434f3b27f771a8d05cb32a64edb9d4f4e00028
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-02-14 21:54:59 +0530

    # service implemented

--- src/app/services/book-api.service.ts
+++ src/app/services/book-api.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  /**
+   * Fetches book data
+   * @param term string
+   * @returns Observable
+   */
+  searchGetCall(term: string): Observable<any> {
+    if (term === '') {
+      return of([]);
+    }
+    return this.http.get(`http://openlibrary.org/search.json?q=${term}`);
+  }
 }
