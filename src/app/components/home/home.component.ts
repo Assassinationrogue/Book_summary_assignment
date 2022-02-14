f37a9bf744736b8b84a63dd50dbeeef70bb3a97e
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-02-14 22:16:13 +0530

    # logic to fetch data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,25 +1,49 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { fromEvent, merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { BookApiService } from 'src/app/services/book-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   isLoading: Boolean = false;
   apiResponse: any = [];
 
-  constructor(public searchInput: ElementRef) { 
+  constructor(
+    public searchInput: ElementRef,
+    private bookService: BookApiService
+  ) {
     this.searchInput = searchInput;
   }
 
   ngOnInit(): void {
-    const keyupEvent$ = fromEvent(this.searchInput?.nativeElement, "keyup");
-    const pasteEvent$ = fromEvent(this.searchInput?.nativeElement, "input");
+    const keyupEvent$ = fromEvent(this.searchInput?.nativeElement, 'keyup');
+    const pasteEvent$ = fromEvent(this.searchInput?.nativeElement, 'input');
     const allEvents$ = merge(keyupEvent$, pasteEvent$);
+
+    allEvents$
+      .pipe(
+        map((event: any) => {
+          return event.target.value;
+        }),
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe((text: string) => {
+        this.isLoading = true;
+        this.bookService.searchGetCall(text).subscribe(
+          (res) => {
+            this.isLoading = false;
+            this.apiResponse = res;
+            console.log(res);
+          },
+          (err) => {
+            this.isLoading = false;
+          }
+        );
+      });
   }
-
-  
-
 }
