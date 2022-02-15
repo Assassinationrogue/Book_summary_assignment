65ddbc12b0740a8d8f9681f2879412b72731ffcc
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-02-15 14:15:03 +0530

    # data formatting

--- src/app/detail/book/book.component.ts
+++ src/app/detail/book/book.component.ts
@@ -2,22 +2,39 @@ import { BookApiService } from 'src/app/services/book-api.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookDetail } from 'src/app/models/book';
+import { tap, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
 })
 export class BookComponent implements OnInit {
   bookDetails: BookDetail;
-  constructor(private router: ActivatedRoute, private bookService: BookApiService) { }
+
+  constructor(
+    private router: ActivatedRoute,
+    private bookService: BookApiService
+  ) {}
 
   ngOnInit(): void {
-    this.bookService.getBookDetails(this.router.snapshot.params.id).subscribe(details=>{
-      this.bookDetails = details;
-      console.log(this.bookDetails)
-    })
-
+    this.bookService
+      .getBookDetails(this.router.snapshot.params.id)
+      .pipe(
+        map((data) => {
+          return ({
+            title: data.title,
+            publishers: data.publishers.join(','),
+            physical_format: data.physical_format,
+            number_of_pages: data.number_of_pages,
+            publish_date: data.publish_date,
+            isbn_10: data.isbn_10.join(','),
+            isbn_13: data.isbn_13.join(','),
+          });
+        })
+      )
+      .subscribe((details) => {
+        this.bookDetails = details;
+      });
   }
-
 }
--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -1,21 +1,21 @@
 export interface BookDetail {
-  authors: [];
-  created: any;
-  identifiers: any;
+  authors?: [];
+  created?: any;
+  identifiers?: any;
   isbn_10: string[];
   isbn_13: string[];
-  key: string;
-  last_modified: any;
-  latest_revision: number;
+  key?: string;
+  last_modified?: any;
+  latest_revision?: number;
   number_of_pages: number;
   physical_format: string;
   publish_date: string;
   publishers: string[];
-  revision: number;
-  subjects: string[];
+  revision?: number;
+  subjects?: string[];
   title: string[];
-  type: any;
-  works: any;
+  type?: any;
+  works?: any;
 }
 
 export interface BookBriefInfo {
